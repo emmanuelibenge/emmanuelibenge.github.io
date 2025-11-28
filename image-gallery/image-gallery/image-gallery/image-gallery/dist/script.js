1ca9ba513e6a2ca5bda459bcba6d5ffd804e8190
emmanuelibenge <157649729+emmanuelibenge@users.noreply.github.com>
2025-11-28 18:43:43 -0500

    Add the togge button for lighten and darken

--- image-gallery/image-gallery/image-gallery/image-gallery/dist/script.js
+++ image-gallery/image-gallery/image-gallery/image-gallery/dist/script.js
@@ -4,30 +4,57 @@ const thumbBar = document.querySelector(".thumb-bar");
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
+
 const images = [
   { filename: "pic1.jpg", alt: "Closeup of a human eye" },
   { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
-  { filename: "Purple and white pansies", alt: "Purple and white pansies" },
-  { filename: "pic4.jpg", alt: "Section of wall from a pharaohs tomb" },
-  { filename: "pic5.jpg", alt: "Large moth on a leaf" }
-]
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
 
-const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/"
+
+const baseURL =
+  "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
 
 for (const image of images) {
-  const newImg = document.createElement("img")
-  newImg.src = baseURL + image.filename
-  newImg.alt = image.alt
-  newImg.tabIndex = "0"
-  thumbBar.appendChild(newImg)
   
-  function updateDisplayedImage(e) {
-  displayedImage.src = e.target.src
-  displayedImage.alt = e.target.alt
+  const newImage = document.createElement("img");
+
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt = image.alt;
+  
+  newImage.tabIndex = "0";
+  
+  thumbBar.appendChild(newImage);
+ 
+  newImage.addEventListener("click", updateDisplayedImage);
+  
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
 }
 
-for (const image of images) {
- newImg.addEventListener("click", updateDisplayedImage)
+
+function updateDisplayedImage(e) {
+  displayedImage.src = e.target.src;
+  displayedImage.alt = e.target.alt;
 }
 
-}
+
+btn.addEventListener("click", () => {
+  
+  if (btn.classList.contains("dark")) {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+  } else {
+    
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
+  }
+ 
+  btn.classList.toggle("dark");
+});
