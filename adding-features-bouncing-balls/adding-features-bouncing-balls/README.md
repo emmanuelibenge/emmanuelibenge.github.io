ba29dbce26013a6247738636d1e6ac29c5eb454f
emmanuelibenge <157649729+emmanuelibenge@users.noreply.github.com>
2025-11-28 20:12:25 -0500

    Set up the functions

---
+++ adding-features-bouncing-balls/adding-features-bouncing-balls/README.md
@@ -0,0 +1,6 @@
+# Adding Features Bouncing Balls
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/emmanuel-libenge/pen/pvyKzRP](https://codepen.io/emmanuel-libenge/pen/pvyKzRP).
+
---
+++ adding-features-bouncing-balls/adding-features-bouncing-balls/dist/script.js
@@ -0,0 +1,83 @@
+// main.js
+
+const para = document.querySelector('p');
+let count = 0;
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0,255)},${random(0,255)},${random(0,255)})`;
+}
+
+class Shape {
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
+    this.color = color;
+    this.size = size;
+    this.exists = true;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if ((this.x + this.size) >= width || (this.x - this.size) <= 0) this.velX = -this.velX;
+    if ((this.y + this.size) >= height || (this.y - this.size) <= 0) this.velY = -this.velY;
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+}
+
+// populate balls
+const balls = [];
+while (balls.length < 25) {
+  const size = random(10,20);
+  const ball = new Ball(
+    random(size, width-size),
+    random(size, height-size),
+    random(-7,7),
+    random(-7,7),
+    randomRGB(),
+    size
+  );
+  balls.push(ball);
+  count++;
+  para.textContent = 'Ball count: ' + count;
+}
+
+function loop() {
+  ctx.fillStyle = 'rgba(0,0,0,0.25)';
+  ctx.fillRect(0,0,width,height);
+
+  for (const ball of balls) {
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+    }
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
