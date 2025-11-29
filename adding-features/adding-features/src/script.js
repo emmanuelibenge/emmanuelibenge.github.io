beca5e6da931fffe83a0f5498543fd92c7bb3f56
emmanuelibenge <157649729+emmanuelibenge@users.noreply.github.com>
2025-11-28 19:40:29 -0500

    Add files via upload

---
+++ adding-features/adding-features/src/script.js
@@ -0,0 +1,49 @@
+
+
+const balls = [];
+const width = window.innerWidth;
+const height = window.innerHeight;
+const ctx = document.querySelector('canvas').getContext('2d');
+
+class Shape {
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+}
+
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, size, color) {
+    super(x, y, velX, velY);
+    this.size = size;
+    this.color = color;
+    this.exists = true;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    
+  }
+}
